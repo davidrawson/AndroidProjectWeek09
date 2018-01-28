fcfcc24515a5dba0315a37ae80b6a0de7ed6cae3
David Rawson <dm.rawson@gmail.com>
2018-01-28 20:29:04 +0000

    Win message shown. Cards moved. Bare bones look.

--- app/src/main/java/example/codeclan/com/playyourcardsright/Game.java
+++ app/src/main/java/example/codeclan/com/playyourcardsright/Game.java
@@ -1,5 +1,7 @@
 package example.codeclan.com.playyourcardsright;
 
+import android.util.Log;
+
 /**
  * Created by davidrawson on 28/01/2018.
  */
@@ -9,17 +11,31 @@ public class Game {
     private Deck deck;
     private int roundNumber;
     private int roundsTotal;
+    private Card firstCard;
+    private Card nextCard;
+    private boolean roundWin;
 
     public Game(int roundsTotal){
         deck = new Deck();
         deck.shuffleCards();
         this.roundNumber = 1;
         this.roundsTotal = roundsTotal;
+        this.firstCard = deck.removeCard();
+        this.nextCard = deck.removeCard();
+
     }
 
 
     public Deck getDeck() {
         return deck;
+    }
+
+    public Card getFirstCard(){
+        return this.firstCard;
+    }
+
+    public Card getNextCard(){
+        return nextCard;
     }
 
     public int getRoundNumber() {
@@ -31,4 +47,31 @@ public class Game {
     }
 
 
+    public boolean checkForRoundWin(Object tag) {
+        if (tag == "higher"){
+            if (this.nextCard.getRank().getValue() > this.firstCard.getRank().getValue()){
+                roundWin = true;
+            }else{
+                roundWin = false;
+            }
+        }
+        if (tag == "lower"){
+            if (this.nextCard.getRank().getValue() < this.firstCard.getRank().getValue()){
+                roundWin = true;
+            }else{
+                roundWin = false;
+            }
+        }
+        Log.d(getClass().toString(), "Correct guess? " +roundWin);
+        return this.roundWin;
+    }
+
+    public void changeFirstCard() {
+        this.firstCard = deck.removeCard();
+    }
+
+    public void moveCards(){
+        this.firstCard = this.nextCard;
+        this.nextCard = deck.removeCard();
+    }
 }
--- app/src/main/java/example/codeclan/com/playyourcardsright/MainActivity.java
+++ app/src/main/java/example/codeclan/com/playyourcardsright/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean firstGo;
     private Deck deck;
     private Game game;
+    private View winMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         higherButton.setTag("higher");
         View lowerButton = findViewById(R.id.lowerButton);
         lowerButton.setTag("lower");
+        winMessage = findViewById(R.id.winMessage);
+        winMessage.setVisibility(INVISIBLE);
 
         Intent intent = getIntent(); // gets the intent that started this activity, the AnswerActivity.
         Bundle extras = intent.getExtras();  // just a bunch of data. All onCreate methods get one.
@@ -41,19 +44,67 @@ public class MainActivity extends AppCompatActivity {
         Log.d(getClass().toString(), "Button " + gameType);
 
 //        if (gameType == "full"){
-            game = new Game(51);
+            game = new Game(5);
 //        }
 //        if (gameType == "five"){
 //            game = new Game(5);
 //        }
 
-        deck = game.getDeck();
-        Card card = deck.removeCard();
+//        deck = game.getDeck();
+//        Card card = deck.removeCard();
 
-        String cardID = card.getImageFile();
+//        String cardID = game.getFirstCard().getImageFile();
+//
+//        int cardPic = getResources().getIdentifier(cardID, "drawable", getPackageName());
 
+        setCards();
+//        firstView = findViewById(R.id.firstCard);
+//        firstView.setImageResource(cardPic);
+//
+//        nextView = findViewById(R.id.nextCard);
+//        nextView.setImageResource(R.drawable.red_back);
+    }
+
+    public void onHigherLowerButtonClick(View button){
+        firstGo = false;
+        View refreshButton = findViewById(R.id.refreshButton);
+        refreshButton.setVisibility(INVISIBLE);
+        Log.d(getClass().toString(), "onHigherLowerButtonClick " + button.getTag());
+        String cardID = game.getNextCard().getImageFile();
         int cardPic = getResources().getIdentifier(cardID, "drawable", getPackageName());
+        nextView = findViewById(R.id.nextCard);
+        nextView.setImageResource(cardPic);
 
+//        removeDownturnedCard();
+        if (game.checkForRoundWin(button.getTag())){
+            winMessage.setVisibility(View.VISIBLE);
+        }else{
+            // loser
+
+        }
+    }
+
+    public void onChangeCardButtonClick(View button){
+        game.changeFirstCard();
+        String cardID = game.getFirstCard().getImageFile();
+        int cardPic = getResources().getIdentifier(cardID, "drawable", getPackageName());
+        if (firstGo = true) {
+            firstView.setImageResource(cardPic);
+        }
+        firstGo = false;
+        button.setVisibility(INVISIBLE);
+    }
+
+    public void onWinMessageClick(View winButton){
+        winButton.setVisibility(INVISIBLE);
+        game.moveCards();
+        setCards();
+        removeDownturnedCard();
+    }
+
+    public void setCards(){
+        String cardID = game.getFirstCard().getImageFile();
+        int cardPic = getResources().getIdentifier(cardID, "drawable", getPackageName());
 
         firstView = findViewById(R.id.firstCard);
         firstView.setImageResource(cardPic);
@@ -62,20 +113,12 @@ public class MainActivity extends AppCompatActivity {
         nextView.setImageResource(R.drawable.red_back);
     }
 
-    public void onHigherLowerButtonClick(View button){
-        firstGo = false;
-        Log.d(getClass().toString(), "onHigherLowerButtonClick " + button.getTag());
 
-    }
-
-    public void onChangeCardButtonClick(View button){
-        String cardID = deck.removeCard().getImageFile();
-        int cardPic = getResources().getIdentifier(cardID, "drawable", getPackageName());
-        if (firstGo = true) {
-            firstView.setImageResource(cardPic);
+    public void removeDownturnedCard() {
+        if (game.getRoundNumber() == 1){
+            View cardImage = findViewById(R.id.card5);
+            cardImage.setVisibility(INVISIBLE);
         }
-        firstGo = false;
-        button.setVisibility(INVISIBLE);
     }
 
 }
