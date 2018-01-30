9277692b1dc51c863654455ba4e2070082f29c57
David Rawson <dm.rawson@gmail.com>
2018-01-30 10:33:33 +0000

    Displays scrollable listvie of names. Displaying the score isn't yet working.

--- app/src/main/java/example/codeclan/com/playyourcardsright/LeaderboardAdapter.java
+++ app/src/main/java/example/codeclan/com/playyourcardsright/LeaderboardAdapter.java
@@ -20,6 +20,25 @@ class LeaderboardAdapter extends ArrayAdapter<LeaderEntry>{
     }
 
 
+    @Override
+    public View getView(int position, View leaderListView, ViewGroup parent){
+        if (leaderListView == null){
+            leaderListView = LayoutInflater.from(getContext()).inflate(R.layout.leaderboard_item, parent, false);
+        }
+        LeaderEntry currentEntry = getItem(position);
+
+        TextView name = leaderListView.findViewById(R.id.name);
+        name.setText(currentEntry.getName());
+
+//        TextView roundsCompleted = leaderListView.findViewById(R.id.roundCompleted);
+//        roundsCompleted.setText(currentEntry.getRound());
+
+        leaderListView.setTag(currentEntry);
+
+        return leaderListView;
+    }
+
+
 }
 
 
