9ff89fe56f66ff4055308da86ad9d469d553b76e
David Rawson <dm.rawson@gmail.com>
2018-02-01 10:44:20 +0000

    Rules formatted correctly.

--- app/src/main/java/example/codeclan/com/playyourcardsright/RulesActivity.java
+++ app/src/main/java/example/codeclan/com/playyourcardsright/RulesActivity.java
@@ -2,7 +2,6 @@ package example.codeclan.com.playyourcardsright;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class RulesActivity extends AppCompatActivity {
 
@@ -10,10 +9,6 @@ public class RulesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
-
-
-        TextView rulesView = findViewById(R.id.rulesText);
-        rulesView.setText("There are five cards laid out, and you are to guess if the next card is higher or lower - ace being the highest card, and two being lowest./nThe first card can be changed if you wish./n/nIf your guess is correct you continue with the next card./n/nCorrectly guessing all cards to the end wins the game./n/nThe Full Deck Game has the same rules but uses the full deck rather than just five cards./n/nRemember, you get nothing for a pair, not in this game.");
 
     }
 }
